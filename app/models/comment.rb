908497a853319e6142b71a441051adcb4ffaff05
bashforger <bash.forger@gmail.com>
2021-01-05 19:04:48 +0500

    Fixed liners issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  validates_associated :user,:post
-  validates :name,:body ,presence: true,length:{in: 3..50}
+  validates_associated :user, :post
+  validates :name, :body, presence: true, length: { in: 3..50 }
 end
