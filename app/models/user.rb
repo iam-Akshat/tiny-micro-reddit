530c3da49007b2de0316c326935253f007ec1caa
Akshat Sethi <masterakshata@gmail.com>
2021-01-05 16:04:28 +0530

    basic data validation for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :username, presence: true, length:{ in: 3..10 }, uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length:{ in: 6..12 }
 end
