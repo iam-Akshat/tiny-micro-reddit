c5a20608e3f6d37b168241db91506fbe26b7a72b
bashforger <bash.forger@gmail.com>
2021-01-05 17:49:45 +0500

    Added Comments section

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
+
+    validates :title, presence: true, length:{ in: 3..20, too_short: "too_short" }
+    validates :body, length:{ maximum: 50 }
 end
---
+++ db/migrate/20210105124447_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.string :name
+      t.text :body
+      t.references :user
+      t.references :post
+
+      t.timestamps
+    end
+  end
+end
